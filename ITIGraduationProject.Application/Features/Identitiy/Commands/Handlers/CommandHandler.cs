e0f73857191471c9402c0cf60b81b3998a95963c
Fatma_Ali <fa5675869@gmail.com>
2026-06-18 14:39:20 +0300

    Refactor Login

---
+++ ITIGraduationProject.Application/Features/Identitiy/Commands/Handlers/CommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using ITIGraduationProject.Application.Bases;
+using ITIGraduationProject.Application.DTOS;
+using ITIGraduationProject.Application.Features.Identitiy.Commands.Models;
+using ITIGraduationProject.Application.Interfaces.IServices.IdentityServices;
+using MediatR;
+using MapsterMapper;
+
+namespace ITIGraduationProject.Application.Features.Identitiy.Commands.Handlers
+{
+    public class CommandHandler :ResponseHandler,
+        IRequestHandler<RegisterCommand, Response<string>>,
+                IRequestHandler<ConfirmEmailCommand, Response<string>>, IRequestHandler<LoginCommand, Response<LoginResponseDTO>>
+
+    {
+        private readonly IIdentityService _identityService;
+        private readonly IMapper _mapper;
+
+        public CommandHandler(IIdentityService identityService, IMapper mapper)
+        {
+            _identityService = identityService;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<string>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            var dto = _mapper.Map<RegisterRequestDTO>(request);
+            return await _identityService.RegisterAsync(dto);
+        }
+        public async Task<Response<string>> Handle(ConfirmEmailCommand request, CancellationToken cancellationToken)
+        {
+            return await _identityService.ConfirmEmailAsync(request.UserId, request.Token);
+        }
+        public async Task<Response<LoginResponseDTO>> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var dto = _mapper.Map<LoginRequestDTO>(request);
+            return await _identityService.LoginAsync(dto);
+        }
+    }
+}
---
+++ ITIGraduationProject.Application/Interfaces/IServices/IdentityServices/IIdentityService.cs
@@ -0,0 +1,20 @@
+﻿using ITIGraduationProject.Application.Bases;
+using ITIGraduationProject.Application.DTOS;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITIGraduationProject.Application.Interfaces.IServices.IdentityServices
+{
+  
+        public interface IIdentityService
+        {
+            Task<Response<string>> RegisterAsync(RegisterRequestDTO request);
+            Task<Response<string>> ConfirmEmailAsync(string userId, string token);
+            Task<Response<LoginResponseDTO>> LoginAsync(LoginRequestDTO request);
+        }
+    
+}
