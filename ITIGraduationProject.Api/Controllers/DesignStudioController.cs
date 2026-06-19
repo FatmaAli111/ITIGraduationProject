4ce9ff1bf2baac974f7f2e20e78d295ec7f5f935
abdalhadydev <abdalhady.dev@gmail.com>
2026-06-20 00:08:55 +0300

    Add DesignStudioController with product retrieval endpoints

---
+++ ITIGraduationProject.Api/Controllers/DesignStudioController.cs
@@ -0,0 +1,39 @@
+﻿using ITIGraduationProject.Application.Features.Studio.Queries.GetProductForCustomization;
+using ITIGraduationProject.Application.Features.Studio.Queries.GetStudioProducts;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITIGraduationProject.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DesignStudioController : ControllerBase
+    {
+        private readonly ISender _mediator;
+
+        public DesignStudioController(ISender mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("products")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<StudioProductListItemDto>))]
+        public async Task<IActionResult> GetStudioProducts(CancellationToken cancellationToken)
+        {
+            var query = new GetStudioProductsQuery();
+            var result = await _mediator.Send(query, cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpGet("products/{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(StudioProductDetailDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetProductForCustomization([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var query = new GetProductForCustomizationQuery(id);
+            var result = await _mediator.Send(query, cancellationToken);
+            return Ok(result);
+        }
+    }
+}
