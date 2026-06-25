a085fd9f768ef3689b18b2d1af1bd0e51fea1e8a
Reem Abdelkader <reemabdelkader97@gmail.com>
2026-06-26 01:53:13 +0300

    chat updates

--- ITIGraduationProject.Application/Features/AiChat/Queries/Handlers/GetChatSessionHistoryQueryHandler.cs
+++ ITIGraduationProject.Application/Features/AiChat/Queries/Handlers/GetChatSessionHistoryQueryHandler.cs
@@ -1,14 +1,15 @@
 ﻿using ITIGraduationProject.Application.Features.AiChat.Queries.Models;
 using ITIGraduationProject.Application.Interfaces.Persistence;
 using ITIGraduationProject.Application.Bases;
-using ITIGraduationProject.Domain.Entities.AIAndModeration;
+using ITIGraduationProject.Application.DTOS.AiChat;
 using MediatR;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ITIGraduationProject.Application.Features.AiChat.Queries.Handlers
 {
-    public class GetChatSessionHistoryQueryHandler : IRequestHandler<GetChatSessionHistoryQuery, Response<AiChatSession>>
+    public class GetChatSessionHistoryQueryHandler : IRequestHandler<GetChatSessionHistoryQuery, Response<ChatHistoryDto>>
     {
         #region Dependency Injection
         private readonly IUnitOfWork _unitOfWork;
@@ -20,14 +21,28 @@ namespace ITIGraduationProject.Application.Features.AiChat.Queries.Handlers
         #endregion
 
         #region Handle Method
-        public async Task<Response<AiChatSession>> Handle(GetChatSessionHistoryQuery request, CancellationToken cancellationToken)
+        public async Task<Response<ChatHistoryDto>> Handle(GetChatSessionHistoryQuery request, CancellationToken cancellationToken)
         {
             var session = await _unitOfWork.AiChatSessions.GetWithMessagesAsync(request.SessionId);
 
             if (session == null)
-                return new Response<AiChatSession>("Chat session not found.");
+                return new Response<ChatHistoryDto>("Chat session not found.");
 
-            return new Response<AiChatSession>(session);
+            var chatHistoryDto = new ChatHistoryDto
+            {
+                SessionId = session.Id,
+                SessionType = session.SessionType.ToString(),
+                CreatedAt = System.DateTime.UtcNow,
+                Messages = session.AiChatMessages.Select(m => new ChatMessageDto
+                {
+                    Id = m.Id,
+                    Sender = m.Sender,
+                    MessageText = m.MessageText,
+                    SentAt = m.SentAt
+                }).OrderBy(m => m.SentAt).ToList()
+            };
+
+            return new Response<ChatHistoryDto>(chatHistoryDto);
         }
         #endregion
     }
--- ITIGraduationProject.Application/Features/AiChat/Queries/Models/GetChatSessionHistoryQuery.cs
+++ ITIGraduationProject.Application/Features/AiChat/Queries/Models/GetChatSessionHistoryQuery.cs
@@ -1,12 +1,13 @@
 ﻿using MediatR;
 using ITIGraduationProject.Application.Bases;
 using System;
-using ITIGraduationProject.Domain.Entities.AIAndModeration;
+using ITIGraduationProject.Application.DTOS.AiChat;
 
 namespace ITIGraduationProject.Application.Features.AiChat.Queries.Models
 {
-    public class GetChatSessionHistoryQuery : IRequest<Response<AiChatSession>>
+    public class GetChatSessionHistoryQuery : IRequest<Response<ChatHistoryDto>>
     {
         public Guid SessionId { get; set; }
+        public Guid RequestingUserId { get; set; }
     }
 }
